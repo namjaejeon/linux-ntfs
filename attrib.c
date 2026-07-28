fdceb16f104dcdd9281bf728fc73fb18aa4798f8
Hyunchul Lee <hyc.lee@gmail.com>
2026-07-28 11:26:46 +0900

    ntfs: protect mapping-pairs attr-list updates with attr_list_persist_lock
    ntfs_attr_update_mapping_pairs() updates several ALEs while rebuilding
    a fragmented attribute's mapping pairs and persists the attribute
    list once after the loop.  Each ALE update is covered by
    attr_list_lock, but the overall publish/persist sequence is not
    serialized against concurrent ntfs_attrlist_entry_add()/rm() on the
    same base inode.
    
    Acquire attr_list_persist_lock once after resolving base_ni whenever
    the call can touch the attribute list, and hold it across every ALE
    update and the deferred ntfs_attrlist_update() call.
    
    Reported-by: Cen Zhang <zzzccc427@gmail.com>
    Link: https://lore.kernel.org/all/20260702161710.2606853-1-zzzccc427@gmail.com/
    Fixes: 495e90fa3348 ("ntfs: update attrib operations")
    Cc: stable@vger.kernel.org
    Tested-by: Cen Zhang <zzzccc427@gmail.com>
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- attrib.c
+++ attrib.c
@@ -4102,7 +4102,7 @@ int ntfs_attr_update_mapping_pairs(struct ntfs_inode *ni, s64 from_vcn)
 	struct attr_record *a;
 	s64 stop_vcn;
 	int err = 0, mp_size, cur_max_mp_size, exp_max_mp_size;
-	bool finished_build, attrlist_changed = false;
+	bool finished_build, attrlist_changed = false, attrlist_locked = false;
 	bool first_updated = false;
 	struct super_block *sb;
 	struct runlist_element *start_rl;
@@ -4127,9 +4127,16 @@ retry:
 	else
 		base_ni = ni;
 
+	if (NInoAttrList(base_ni) && ni->type != AT_ATTRIBUTE_LIST) {
+		mutex_lock(&base_ni->attr_list_persist_lock);
+		attrlist_locked = true;
+	}
+
 	ctx = ntfs_attr_get_search_ctx(base_ni, NULL);
 	if (!ctx) {
 		ntfs_error(sb, "%s: Failed to get search context", __func__);
+		if (attrlist_locked)
+			mutex_unlock(&base_ni->attr_list_persist_lock);
 		return -ENOMEM;
 	}
 
@@ -4203,6 +4210,10 @@ retry:
 		err = ntfs_attr_update_meta(a, ni, m, ctx);
 		if (err < 0) {
 			if (err == -EAGAIN) {
+				if (attrlist_locked) {
+					mutex_unlock(&base_ni->attr_list_persist_lock);
+					attrlist_locked = false;
+				}
 				ntfs_attr_put_search_ctx(ctx);
 				goto retry;
 			}
@@ -4239,6 +4250,10 @@ retry:
 			 * attributes and try again.
 			 */
 			if (ni->type == AT_ATTRIBUTE_LIST) {
+				if (WARN_ON_ONCE(attrlist_locked)) {
+					mutex_unlock(&base_ni->attr_list_persist_lock);
+					attrlist_locked = false;
+				}
 				ntfs_attr_put_search_ctx(ctx);
 				if (ntfs_inode_free_space(base_ni, mp_size -
 							cur_max_mp_size)) {
@@ -4252,6 +4267,10 @@ retry:
 
 			/* Add attribute list if it isn't present, and retry. */
 			if (!NInoAttrList(base_ni)) {
+				if (WARN_ON_ONCE(attrlist_locked)) {
+					mutex_unlock(&base_ni->attr_list_persist_lock);
+					attrlist_locked = false;
+				}
 				ntfs_attr_put_search_ctx(ctx);
 				if (ntfs_inode_add_attrlist(base_ni)) {
 					ntfs_error(sb, "Can not add attrlist");
@@ -4365,9 +4384,13 @@ retry:
 
 	if (attrlist_changed) {
 		err = ntfs_attrlist_update(base_ni);
-		if (err)
-			goto put_err_out;
 	}
+	if (attrlist_locked) {
+		mutex_unlock(&base_ni->attr_list_persist_lock);
+		attrlist_locked = false;
+	}
+	if (attrlist_changed && err)
+		goto put_err_out;
 
 	/* Deallocate not used attribute extents and return with success. */
 	if (finished_build) {
@@ -4495,6 +4518,8 @@ out:
 	return 0;
 
 put_err_out:
+	if (attrlist_locked)
+		mutex_unlock(&base_ni->attr_list_persist_lock);
 	if (ctx)
 		ntfs_attr_put_search_ctx(ctx);
 	return err;
