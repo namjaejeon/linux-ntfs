4eed4b34bc92e8c255319eb1aae8ff5fd08c2fb9
Hyunchul Lee <hyc.lee@gmail.com>
2026-09-14 09:01:53 +0900

    ntfs: serialize raw MFT updates against folio writeback
    Because MFT folio writeback can remain active until its asynchronous BIO
    completes, raw MFT record formatting and allocation must not modify the
    folio while it is still used as a BIO source.
    
    Wait for existing folio writeback after taking the folio lock in both
    raw MFT update paths.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- mft.c
+++ mft.c
@@ -2390,6 +2390,8 @@ static int ntfs_mft_record_format(const struct ntfs_volume *vol, const s64 mft_n
 		return PTR_ERR(folio);
 	}
 	folio_lock(folio);
+	if (folio_test_writeback(folio))
+		folio_wait_writeback(folio);
 	folio_clear_uptodate(folio);
 	m = (struct mft_record *)((u8 *)kmap_local_folio(folio, 0) + ofs);
 #else
@@ -2400,6 +2402,7 @@ static int ntfs_mft_record_format(const struct ntfs_volume *vol, const s64 mft_n
 		return PTR_ERR(page);
 	}
 	lock_page(page);
+	wait_on_page_writeback(page);
 	BUG_ON(!PageUptodate(page));
 	ClearPageUptodate(page);
 	m = (struct mft_record *)((u8 *)page_address(page) + ofs);
@@ -2940,6 +2943,8 @@ mft_rec_already_initialized:
 		goto undo_mftbmp_alloc;
 	}
 	folio_lock(folio);
+	if (folio_test_writeback(folio))
+		folio_wait_writeback(folio);
 	folio_clear_uptodate(folio);
 	m = (struct mft_record *)((u8 *)kmap_local_folio(folio, 0) + ofs);
 #else
@@ -2951,6 +2956,7 @@ mft_rec_already_initialized:
 		goto undo_mftbmp_alloc;
 	}
 	lock_page(page);
+	wait_on_page_writeback(page);
 	BUG_ON(!PageUptodate(page));
 	ClearPageUptodate(page);
 	m = (struct mft_record *)((u8 *)page_address(page) + ofs);
